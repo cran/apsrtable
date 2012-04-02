7fbb01dc3d63cd79a2b6fd9d2a6e16cc16dd9938
Michael Malecki <malecki@wustl.edu>
2012-04-02 00:00:00 +0000

    version 0.8-8

--- apsrtable-Ex.R
+++ apsrtable-Ex.R
@@ -90,10 +90,10 @@ flush(stderr()); flush(stdout())
 ### Name: modelInfo
 ### Title: Model fit and diagnostic functions for output
 ### Aliases: modelInfo modelInfo,summary.lm-method
-###   modelInfo,summary.glm-method modelInfo,summary.tobit-method
-###   modelInfo,summary.gee-method modelInfo,summary.coxph-method
-###   modelInfo,summary.clogit-method modelInfo,summary.negbin-method
-###   modelInfo,summary.lrm-method
+###   modelInfo,summary.glm-method modelInfo,summary.svyglm-method
+###   modelInfo,summary.tobit-method modelInfo,summary.gee-method
+###   modelInfo,summary.coxph-method modelInfo,summary.clogit-method
+###   modelInfo,summary.negbin-method modelInfo,summary.lrm-method
 
 ### ** Examples
  
